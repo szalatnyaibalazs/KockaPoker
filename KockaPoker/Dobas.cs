d4c0ac24195103da7207c8fa10785bd55b0a6025
szalatnyaibalazs <szalatnyaibalazs@gamil.com>
2020-11-04 14:51:56 +0100

    Add project files.

---
+++ KockaPoker/Dobas.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KockaPoker
+{
+    class Dobas
+    {
+        int[] kockak = new int[5];
+
+        public Dobas(int k1 ,int k2,int k3, int k4, int k5)
+        {
+            kockak[0] = k1;
+            kockak[1] = k2;
+            kockak[2] = k3;
+            kockak[3] = k4;
+            kockak[4] = k5;
+        }
+        public void EgyDobas()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < kockak.Length; i++)
+            {
+                kockak[i] = rnd.Next(1, 7);
+            }
+        }
+        public void KiIras()
+        {
+            foreach (var k in kockak)
+            {
+                Console.Write($"{k} ");
+            }
+        }
+        public string Erteke()
+        {
+            Dictionary<int, int> eredmeny = new Dictionary<int, int>();
+
+            for (int i = 0; i <=6; i++)
+            {
+                eredmeny.Add(i,0);
+            }
+
+            foreach (var k in kockak)
+            {
+                eredmeny[k]++;
+            }
+            if (eredmeny.ContainsValue(2) && eredmeny.ContainsValue(3))
+            {
+                return "Full";
+            }
+            else if (eredmeny.ContainsValue(3))
+            {
+                return "Drill";
+            }
+            else if (eredmeny.ContainsValue(2)&&!eredmeny.ContainsValue(3))
+            {
+                return "Pár";
+            }
+            else if (eredmeny.ContainsValue(4))
+            {
+                return "Póker";
+            }
+            else if (eredmeny.ContainsValue(5))
+            {
+                return "Nagypóker";
+            }
+            else if (eredmeny.ContainsValue(1)
+                && !eredmeny.ContainsValue(4) 
+                && !eredmeny.ContainsValue(3) 
+                && !eredmeny.ContainsValue(2) 
+                && !eredmeny.ContainsValue(5))
+            {
+                return "Kis sor";
+            }
+            else if (true)
+            {
+
+            }
+            else
+            {
+                return "Szemét";
+            }
+        }
+    }
+}
---
+++ KockaPoker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KockaPoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dobas d = new Dobas(1,5,1,5,3);
+            //d.EgyDobas();
+            d.KiIras();
+            Console.WriteLine(d.Erteke());
+
+            Console.ReadKey();
+        }
+    }
+}
